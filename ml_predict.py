1356d431a6ab860c31793754748b864b0920f01c
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-11-05 17:13:25 -0300

    atualizando

--- ml_predict.py
+++ ml_predict.py
@@ -6,7 +6,7 @@
 import joblib
 import numpy as np
 
-modelo = joblib.load("classificacao.joblib")
+modelo = joblib.load("../sprint4-chatbot/models/classificacao.joblib")
 
 # ------------------------------------------------------------
 # Função de predição
